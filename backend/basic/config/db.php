5d406ff86a90268165dbcf845822cdd7a845bd08
lucas93c <lucascabralpr@gmail.com>
2023-09-21 18:30:15 -0300

    Punto Nº 3

--- backend/basic/config/db.php
+++ backend/basic/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'pgsql:host='.$_ENV['HOST'].';dbname='.$_ENV['DB'],
+    'username' => $_ENV['USER'],
+    'password' => $_ENV['PASSWORD'],
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
